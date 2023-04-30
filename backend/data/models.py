6d307fbd04304de3b92e5321a35545312b7dabff
Jakub Šiagi <jakub.siagi@gmail.com>
2023-04-30 12:33:54 +0200

    Log users out when login restriction is changed

--- backend/data/models.py
+++ backend/data/models.py
@@ -1,6 +1,9 @@
 import json
 
+from django.contrib.sessions.models import Session
 from django.db import models
+
+from users.models import UserToken
 
 
 class Setting(models.Model):
@@ -86,3 +89,14 @@ class AuthRestriction(models.Model):
 
     def __str__(self):
         return self.type + (' (enabled)' if self.restricted else ' (disabled)')
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.type == 'login':
+            # Invalidate all user tokens when login restriction is changed (for security reasons)
+            UserToken.objects.all().update(invalid=True)
+            # Log out all users when login restriction is changed (for security reasons)
+            Session.objects.all().delete()
+
+        super().save(force_insert, force_update, using, update_fields)
