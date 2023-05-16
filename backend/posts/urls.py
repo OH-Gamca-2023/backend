8e98927158959652e4968becad3306ddf950397c
Jakub Šiagi <jakub.siagi@gmail.com>
2023-05-16 08:58:49 +0200

    Fix urlpatterns order

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -4,6 +4,6 @@ from posts import views
 
 urlpatterns = [
     path('', views.PostListAPIView.as_view()),
-    path('<str:pk>/', views.PostDetailAPIView.as_view()),
     path('tags/', views.TagListAPIView.as_view()),
+    path('<str:pk>/', views.PostDetailAPIView.as_view()),
 ]
