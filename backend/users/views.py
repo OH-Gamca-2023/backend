cceb67e5a44dae5babf18c4adb2fe241f1a5320a
Jakub Šiagi <jakub.siagi@gmail.com>
2023-05-16 09:29:57 +0200

    Convert views to class based

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ import json
 import re
 
 from django.http import JsonResponse, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
 
 from messages.error import not_authenticated, invalid_method, client_error
 
@@ -10,15 +10,18 @@ from data.permissions import profile_edit_permission
 from .serializers import *
 
 
-@csrf_exempt
-def current_user(request):
-    if not request.user.is_authenticated:
-        return not_authenticated()
+class CurrentUserAPIView(APIView):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return not_authenticated()
 
-    if request.method == 'GET':
         serializer = UserSerializer(request.user)
         return JsonResponse(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return not_authenticated()
+
         data = json.loads(request.body)
         # validate that the user is allowed to edit all the fields present in the request
         for field in data:
@@ -41,72 +44,115 @@ def current_user(request):
         return JsonResponse(serializer.data)
 
 
-def user_permissions(request):
-    if not request.user.is_authenticated:
-        return not_authenticated()
+class UserPermissionAPIView(APIView):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return not_authenticated()
+        user_type = request.user.type()
 
-    user_type = request.user.type()
+        response = {
+            'user_type': user_type,
+            'staff': request.user.is_staff,
+            'admin': request.user.is_admin,
+            'superuser': request.user.is_superuser,
+            'permissions': [(perm.content_type.app_label + '.' + perm.codename) for perm in request.user.get_all_permissions()],
+            'profile_edit': []
+        }
 
-    response = {
-        'user_type': user_type,
-        'staff': request.user.is_staff,
-        'admin': request.user.is_admin,
-        'superuser': request.user.is_superuser,
-        'permissions': [(perm.content_type.app_label + '.' + perm.codename) for perm in request.user.get_all_permissions()],
-    }
+        if user_type in profile_edit_permission:
+            response['profile_edit'] = profile_edit_permission[user_type]
 
-    if user_type in profile_edit_permission:
-        response['profile_edit'] = profile_edit_permission[user_type]
-    else:
-        response['profile_edit'] = []
-
-    return JsonResponse(response)
-
-
-@csrf_exempt
-def change_password(request):
-    if not request.user.is_authenticated:
-        return not_authenticated()
-    if request.method != 'POST' and request.method != 'PUT':
-        return invalid_method(['POST', 'PUT'])
-
-    if not request.user.is_staff:
-        return client_error(403, 'no_permission.change', 'your password')
-
-    data = json.loads(request.body)
-
-    if 'new_password' not in data:
-        return client_error(400, 'required', 'New password')
-    if request.user.has_password() and 'old_password' not in data:
-        return client_error(400, 'required', 'Old password')
-
-    if request.user.has_password() and not request.user.check_password(data['old_password']):
-        return client_error(400, 'incorrect', 'Old password')
-
-    if request.user.has_password() and data['new_password'] == data['old_password']:
-        return client_error(409, 'not_the_same', 'New password', 'old password')
-    if len(data['new_password']) < 8:
-        return client_error(409, 'too_short', 'New password', 8)
-    if len(data['new_password']) > 128:
-        return client_error(409, 'too_long', 'New password', 128)
-    if not (any(char.isdigit() for char in data['new_password']) or any(not char.isalnum() for char in data['new_password'])):
-        return client_error(409, 'password.no_not_alpha')
-    if not any(char.isalpha() for char in data['new_password']):
-        return client_error(409, 'password.no_alpha')
-
-    request.user.set_password(data['new_password'])
-    request.user.save()
-
-    return HttpResponse(status=204)
+        return JsonResponse(response)
 
 
-def classes(request):
-    serializer = ClazzSerializer(Clazz.objects.all(), many=True)
+class PasswordChangeAPIView(APIView):
 
-    return JsonResponse(serializer.data, safe=False)
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return not_authenticated()
+
+        if not request.user.is_staff:
+            return client_error(403, 'no_permission.change', 'your password')
+
+        data = json.loads(request.body)
+
+        error = self.validate(request, data)
+        if error is not None:
+            return error
+
+        if request.user.has_password():
+            return client_error(405, 'custom', 'POST is not allowed if the user already has a password, use PUT instead')
+
+        request.user.set_password(data['new_password'])
+        request.user.save()
+
+        return HttpResponse(status=204)
+
+    def put(self, request):
+        if not request.user.is_authenticated:
+            return not_authenticated()
+
+        data = json.loads(request.body)
+
+        error = self.validate(request, data)
+        if error is not None:
+            return error
+
+        if not request.user.has_password():
+            return client_error(405, 'custom', 'PUT is not allowed if the user does not have a password, use POST '
+                                               'instead')
+
+        request.user.set_password(data['new_password'])
+        request.user.save()
+
+        return HttpResponse(status=204)
+
+    def validate(self, request, data):
+        if 'new_password' not in data:
+            return client_error(400, 'required', 'New password')
+        if request.user.has_password() and 'old_password' not in data:
+            return client_error(400, 'required', 'Old password')
+
+        if request.user.has_password() and not request.user.check_password(data['old_password']):
+            return client_error(400, 'incorrect', 'Old password')
+
+        if request.user.has_password() and data['new_password'] == data['old_password']:
+            return client_error(409, 'not_the_same', 'New password', 'old password')
+        if len(data['new_password']) < 8:
+            return client_error(409, 'too_short', 'New password', 8)
+        if len(data['new_password']) > 128:
+            return client_error(409, 'too_long', 'New password', 128)
+        if not (any(char.isdigit() for char in data['new_password']) or any(not char.isalnum() for char in data['new_password'])):
+            return client_error(409, 'password.no_not_alpha')
+        if not any(char.isalpha() for char in data['new_password']):
+            return client_error(409, 'password.no_alpha')
+
+        return None
 
 
-def grades(request):
-    serializer = GradeSerializer(Grade.objects.all(), many=True)
+class ClassListAPIView(APIView):
+    def get(self, request):
+        serializer = ClazzSerializer(Clazz.objects.all(), many=True)
 
-    return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False)
+
+
+class ClassDetailAPIView(APIView):
+    def get(self, request, pk):
+        serializer = ClazzSerializer(Clazz.objects.get(pk=pk))
+
+        return JsonResponse(serializer.data, safe=False)
+
+
+class GradeListAPIView(APIView):
+    def get(self, request):
+        serializer = GradeSerializer(Grade.objects.all(), many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+
+
+class GradeDetailAPIView(APIView):
+    def get(self, request, pk):
+        serializer = GradeSerializer(Grade.objects.get(pk=pk))
+
+        return JsonResponse(serializer.data, safe=False)
