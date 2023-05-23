2f463db7c24064a9382cf6d4c0302a7f9a8116c5
Jakub Šiagi <jakub.siagi@gmail.com>
2023-05-23 15:37:25 +0200

    Convert disciplines to viewsets

--- backend/disciplines/views.py
+++ backend/disciplines/views.py
@@ -1,24 +1,22 @@
 from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 from disciplines.models import Category, Discipline, Result
 from disciplines.serializers import CategorySerializer, DisciplineSerializer, ResultSerializer
 
 
-class CategoriesView(generics.ListAPIView):
-    queryset = Category.objects.all()
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    model = Category
     serializer_class = CategorySerializer
-
-
-class CategoryDetailView(generics.RetrieveAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
 
 
-class DisciplinesView(generics.ListAPIView):
+class DisciplineViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = DisciplineSerializer
     pagination_class = LimitOffsetPagination
     filter_backends = [DjangoFilterBackend, SearchFilter]
@@ -30,18 +28,16 @@ class DisciplinesView(generics.ListAPIView):
             return Discipline.objects.all()
         return Discipline.objects.filter(Q(date_published=True) | Q(details_published=True) | Q(results_published=True))
 
-
-class DisciplineDetailView(generics.RetrieveAPIView):
-    serializer_class = DisciplineSerializer
-
-    def get_queryset(self):
-        if self.request.user.is_authenticated and self.request.user.is_staff:
-            return Discipline.objects.all()
-        return Discipline.objects.filter(Q(date_published=True) | Q(details_published=True) | Q(results_published=True))
+    @action(detail=True)
+    def results(self, request):
+        discipline = self.get_object()
+        queryset = Result.objects.filter(discipline=discipline)
+        serializer = ResultSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class ResultsViewSet(viewsets.ReadOnlyModelViewSet):
     model = Result
     serializer_class = ResultSerializer
     pagination_class = LimitOffsetPagination
-    queryset = Result.objects.all()
+    queryset = Result.objects.filter(discipline__results_published=True)
