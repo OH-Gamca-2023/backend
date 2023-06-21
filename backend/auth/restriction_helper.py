9e95005404e6fc8e5ed7c9cfcd2248872068f5c2
Jakub Šiagi <jakub.siagi@gmail.com>
2023-06-21 09:59:55 +0200

    Fix auth bug

--- backend/auth/restriction_helper.py
+++ backend/auth/restriction_helper.py
@@ -10,17 +10,19 @@ def is_allowed(request, type, user=None, department=None):
         if not restriction.restricted:
             return True, ""
 
+        user_str = f"[{user.username}#{user.id}]" if user else "[anon]"
+
         if user:
             if restriction.bypass_staff and user.is_staff:
-                return True, f"Restriction {type} bypassed for staff user [{user.username}#{user.id}]."
+                return True, f"Restriction {type} bypassed for staff user {user_str}."
             if restriction.bypass_superuser and user.is_superuser:
-                return True, f"Restriction {type} bypassed for superuser [{user.username}#{user.id}]."
+                return True, f"Restriction {type} bypassed for superuser {user_str}."
         if department:
             if department in restriction.bypass_department.split(','):
-                return True, f"Restriction {type} bypassed for user [{user.username}#{user.id}] in department {department}."
+                return True, f"Restriction {type} bypassed for user {user_str} in department {department}."
         if restriction.bypass_ip:
             if request.META['REMOTE_ADDR'] in restriction.bypass_ip.split(','):
-                return True, f"Restriction {type} bypassed for user [{user.username}#{user.id}] with IP {request.META['REMOTE_ADDR']}."
+                return True, f"Restriction {type} bypassed for user {user_str} with IP {request.META['REMOTE_ADDR']}."
 
         return False, restriction.message
     except AuthRestriction.DoesNotExist:
