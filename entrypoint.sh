69f3c1fbf225d6bf675c2e5dc269f2c12f76040e
Jakub Šiagi <jakub.siagi@gmail.com>
2023-08-21 20:17:07 +0200

    Don't migrate on huey start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ "${1:-prod}" = "huey" ]; then
+  exec python manage.py run_huey
+  exit 0
+fi
+
 python manage.py wait_for_database
 python manage.py migrate --force-color
 
 if [ "${1:-prod}" = "dev" ]; then
   exec python manage.py runserver 0.0.0.0:8000 --force-color
-elif [ "${1:-prod}" = "huey" ]; then
-  exec python manage.py run_huey
 else
   python manage.py collectstatic --no-input
   exec gunicorn --bind 0.0.0.0:8000 --access-logfile - --log-file - --error-logfile - --workers 4 backend.wsgi
