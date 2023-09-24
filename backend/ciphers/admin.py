135c9556eb2f3cf5e7057c48dcb6d27a46ab5e1d
Jakub Šiagi <jakub.siagi@gmail.com>
2023-09-24 15:08:36 +0200

    Add cipher results generation

--- backend/ciphers/admin.py
+++ backend/ciphers/admin.py
@@ -1,10 +1,15 @@
-from django.contrib import admin
+import functools
+
+from django.contrib import admin, messages
+from django.http import HttpResponse
+from django_object_actions import DjangoObjectActions, action
 
 from .models import *
+from ..users.models import User
 
 
 @admin.register(Cipher)
-class CipherAdmin(admin.ModelAdmin):
+class CipherAdmin(DjangoObjectActions, admin.ModelAdmin):
     list_display = ('name', 'start', 'end', 'started', 'hint_visible', 'has_ended')
     list_filter = ('start', 'end')
     search_fields = ('name',)
@@ -43,9 +48,70 @@ class CipherAdmin(admin.ModelAdmin):
 
         if not request.user.has_perm("ciphers.change_cipher_advanced"):
             base += ('ignore_case', 'ignore_intermediate_whitespace', 'ignore_trailing_leading_whitespace',
-                     'ignore_accents', 'submission_delay')
+                     'ignore_accents', 'submission_delay', 'max_submissions_per_day')
 
         return base
+
+    @action()
+    def results(self, request, queryset):
+        individual_submitters = Submission.objects.filter(submitted_by__individual_cipher_solving=True).values_list(
+            'submitted_by', flat=True).distinct()
+        clazz_submitters = Submission.objects.filter(submitted_by__individual_cipher_solving=False).values_list(
+            'clazz', flat=True).distinct()
+
+        def get_results(submitters, individual):
+            results = []
+            for submitter in submitters:
+                result_obj = {
+                    'submitter': User.objects.get(pk=submitter).username if individual else Clazz.objects.get(pk=submitter).name,
+                    'solved': Submission.objects.filter(submitted_by=submitter, correct=True)
+                    .values_list('cipher', flat=True).distinct(),
+                    'days_until_solved': 0,
+                    'solved_before_hint': Submission.objects.filter(submitted_by=submitter, correct=True, after_hint=False)
+                    .values_list('cipher', flat=True).distinct().count(),
+                    'wrong': Submission.objects.filter(submitted_by=submitter, correct=False).count(),
+                    'time_until_solved': 0,
+                    'individual': individual
+                }
+
+                for cipher in result_obj['solved']:
+                    solving_submissions = Submission.objects.filter(submitted_by=submitter, cipher=cipher, correct=True)
+                    cipher = Cipher.objects.get(pk=cipher)
+                    result_obj['days_until_solved'] += (solving_submissions.order_by('time').first().time - cipher.start).days
+                    result_obj['time_until_solved'] += int((solving_submissions.order_by('time').first().time - cipher.start).total_seconds())
+
+                result_obj['solved'] = result_obj['solved'].count()
+                results.append(result_obj)
+            return results
+
+        def sort_results(obj1, obj2):
+            # higher is better: solved, solved_before_hint
+            # lower is better: days_until_solved, wrong, time_until_solved
+            if obj1['solved'] != obj2['solved']:
+                return obj2['solved'] - obj1['solved']
+            elif obj1['days_until_solved'] != obj2['days_until_solved']:
+                return obj1['days_until_solved'] - obj2['days_until_solved']
+            elif obj1['solved_before_hint'] != obj2['solved_before_hint']:
+                return obj2['solved_before_hint'] - obj1['solved_before_hint']
+            elif obj1['wrong'] != obj2['wrong']:
+                return obj1['wrong'] - obj2['wrong']
+            else:
+                return obj1['time_until_solved'] - obj2['time_until_solved']
+
+        results = get_results(individual_submitters, True) + get_results(clazz_submitters, False)
+        results.sort(key=functools.cmp_to_key(sort_results))
+
+        # generate csv
+        csv = 'submitter,solved,days_until_solved,solved_before_hint,wrong,time_until_solved,individual\n'
+        for result in results:
+            csv += f'{result["submitter"]},{result["solved"]},{result["days_until_solved"]},{result["solved_before_hint"]},{result["wrong"]},{result["time_until_solved"]},{result["individual"]}\n'
+
+        # return as csv attachment
+        response = HttpResponse(csv, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="results.csv"'
+        return response
+
+    changelist_actions = ('results',)
 
 
 @admin.register(Submission)
